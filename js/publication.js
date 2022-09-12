7b9fb9a80c636099e9edc47a0cdc8c3a41a2f1c6
Bo-Han Chen <66907587+bchen-tw@users.noreply.github.com>
2022-09-13 02:24:25 +0800

    update about section
    update about section

--- js/publication.js
+++ js/publication.js
@@ -2,7 +2,7 @@ var paper_data = [
     {
         img: 'assets/img/journal/opt-express.jpg',
         title: 'Towards Stimulated Raman Scattering Spectro-Microscopy Across the Entire Raman Active Region Using Multiple-plate Continuum',
-        author:'Guan-Jie Huang, Pei-Chen Lai, Ming-Wei Shen, Jia-Xuan Su, Jhan-Yu Guo, Kuo-Chuan Chao, Peng Lin, Ji-Xin Cheng, Li-An Chu, Ann-Shyn Chiang, <span style="color: rgb(31, 85, 143);"><u><strong>Bo-Han Chen</strong></u></span>, Chih-Hsuan Lu, Shi-Wei Chu and Shang-Da Yang',
+        author:'Guan-Jie Huang, Pei-Chen Lai, Ming-Wei Shen, Jia-Xuan Su, Jhan-Yu Guo, Kuo-Chuan Chao, Peng Lin, Ji-Xin Cheng, Li-An Chu, Ann-Shyn Chiang, <span style="color: rgb(31, 85, 143);"><u>Bo-Han Chen</u></span>, Chih-Hsuan Lu, Shi-Wei Chu and Shang-Da Yang',
         journal: 'Optics Express',
         volume: '',
         issue: '',
@@ -14,19 +14,19 @@ var paper_data = [
     {
         img: 'assets/img/journal/annalen-der-physik.webp',
         title: 'Free-electron tomography of few-cycle optical waveforms',
-        author:'Yuya Morimoto, <span style="color: rgb(31, 85, 143);"><u><strong>Bo-Han Chen</strong></u></span> and Peter Baum',
+        author:'Yuya Morimoto, <span style="color: rgb(31, 85, 143);"><u>Bo-Han Chen</u></span> and Peter Baum',
         journal: 'Annalen der Physik',
-        volume: '',
-        issue: '',
-        page: '',
-        year: '2022, Accepted',
-        published: false,
-        link: 'https://onlinelibrary.wiley.com/doi/full/10.1002/andp.202200193'
+        volume: '534',
+        issue: '9',
+        page: '2200193',
+        year: '2022',
+        published: true,
+        link: 'https://doi.org/10.1002/andp.202200193'
     },
     {
         img: 'assets/img/journal/opt-lett.jpg',
         title: 'Vortex beam assisted energy up-scaling for multiple-plate compression with a single spiral phase plate',
-        author:'<span style="color: rgb(31, 85, 143);"><u><strong>Bo-Han Chen</strong></u></span>, Hsing-Wei Huang, Ren-Siang Ye, Chih-Hsuan Lu, Kai Chen, and Shang-Da Yang',
+        author:'<span style="color: rgb(31, 85, 143);"><u>Bo-Han Chen</u></span>, Hsing-Wei Huang, Ren-Siang Ye, Chih-Hsuan Lu, Kai Chen, and Shang-Da Yang',
         journal: 'Optics Letters',
         volume: '47',
         issue: '17',
@@ -38,7 +38,7 @@ var paper_data = [
     {
         img: 'assets/img/journal/no-cover.png',
         title: 'Double-Pass Multiple-Plate Continuum for High-Temporal-Contrast Nonlinear Pulse Compression',
-        author:'<span style="color: rgb(31, 85, 143);"><u><strong>Bo-Han Chen</strong></u></span>, Jia-Xuan Su, Jhan-Yu Guo, Kai Chen, Shi-Wei Chu, Hsuan-Hao Lu, Chih-Hsuan Lu and Shang-Da Yang',
+        author:'<span style="color: rgb(31, 85, 143);"><u>Bo-Han Chen</u></span>, Jia-Xuan Su, Jhan-Yu Guo, Kai Chen, Shi-Wei Chu, Hsuan-Hao Lu, Chih-Hsuan Lu and Shang-Da Yang',
         journal: 'Frontiers in Photonics',
         volume: '3',
         issue: false,
@@ -50,7 +50,7 @@ var paper_data = [
     {
         img: 'assets/img/journal/optica.jpg',
         title: 'Asymmetric single-cycle control of valence electron motion in polar chemical bonds',
-        author:'Yuya Morimoto, Yasushi Shinohara, Mizuki Tani, <span style="color: rgb(31, 85, 143);"><u><strong>Bo-Han Chen</strong></u></span>, Kenichi L. Ishikawa, and Peter Baum',
+        author:'Yuya Morimoto, Yasushi Shinohara, Mizuki Tani, <span style="color: rgb(31, 85, 143);"><u>Bo-Han Chen</u></span>, Kenichi L. Ishikawa, and Peter Baum',
         journal: 'Optica',
         volume: '8',
         issue: '3',
@@ -62,7 +62,7 @@ var paper_data = [
     {
         img: 'assets/img/journal/journal-physics-b.gif',
         title: 'Broadband terahertz solid-state emitter driven by Yb:YAG thin-disk oscillator',
-        author:'Gaia Barbiero, Haochuan Wang, Jonathan Brons, <span style="color: rgb(31, 85, 143);"><u><strong>Bo-Han Chen</strong></u></span>, Vladimir Pervak and Hanieh Fattahi',
+        author:'Gaia Barbiero, Haochuan Wang, Jonathan Brons, <span style="color: rgb(31, 85, 143);"><u>Bo-Han Chen</u></span>, Vladimir Pervak and Hanieh Fattahi',
         journal: 'Journal of Physics B: Atomic, Molecular and Optical Physics',
         volume: '53',
         issue: '12',
@@ -74,7 +74,7 @@ var paper_data = [
     {
         img: 'assets/img/journal/opt-lett.jpg',
         title: 'Second-harmonic generation and self-phase modulation of few-cycle mid-infrared pulses',
-        author:'<span style="color: rgb(31, 85, 143);"><u><strong>Bo-Han Chen</strong></u></span>, Christina Hofer, Ioachim Pupeza, and Peter Baum',
+        author:'<span style="color: rgb(31, 85, 143);"><u>Bo-Han Chen</u></span>, Christina Hofer, Ioachim Pupeza, and Peter Baum',
         journal: 'Optics Letters',
         volume: '44',
         issue: '16',
@@ -86,7 +86,7 @@ var paper_data = [
     {
         img: 'assets/img/journal/opt-express.jpg',
         title: 'Octave-spanning single-cycle middle-infrared generation through optical parametric amplification in LiGaS<sub>2</sub>',
-        author:'<span style="color: rgb(31, 85, 143);"><u><strong>Bo-Han Chen</strong></u></span>, Emanuel Wittmann, Yuya Morimoto, Peter Baum, and Eberhard Riedle',
+        author:'<span style="color: rgb(31, 85, 143);"><u>Bo-Han Chen</u></span>, Emanuel Wittmann, Yuya Morimoto, Peter Baum, and Eberhard Riedle',
         journal: 'Optics Express',
         volume: '27',
         issue: '15',
@@ -98,7 +98,7 @@ var paper_data = [
     {
         img: 'assets/img/journal/opt-lett.jpg',
         title: 'Efficient middle-infrared generation in LiGaS<sub>2</sub> by simultaneous spectral broadening and difference-frequency generation',
-        author:'<span style="color: rgb(31, 85, 143);"><u><strong>Bo-Han Chen</strong></u></span>, Tamas Nagy, and Peter Baum',
+        author:'<span style="color: rgb(31, 85, 143);"><u>Bo-Han Chen</u></span>, Tamas Nagy, and Peter Baum',
         journal: 'Optics Letters',
         volume: '43',
         issue: '8',
@@ -110,7 +110,7 @@ var paper_data = [
     {
         img: 'assets/img/journal/opt-express.jpg',
         title: 'Compression of picosecond pulses from a thin-disk laser to 30fs at 4W average power',
-        author:'<span style="color: rgb(31, 85, 143);"><u><strong>Bo-Han Chen</strong></u></span>, Martin Kretschmar, Dominik Ehberger, Andreas Blumenstein, Peter Simon, Peter Baum, and Tamas Nagy',
+        author:'<span style="color: rgb(31, 85, 143);"><u>Bo-Han Chen</u></span>, Martin Kretschmar, Dominik Ehberger, Andreas Blumenstein, Peter Simon, Peter Baum, and Tamas Nagy',
         journal: 'Optics Express',
         volume: '26',
         issue: '4',
@@ -122,7 +122,7 @@ var paper_data = [
     {
         img: 'assets/img/journal/optica.jpg',
         title: 'Generation of intense supercontinuum in condensed media',
-        author:'Chih-Hsuan Lu, Yu-Jung Tsou, Hong-Yu Chen, <span style="color: rgb(31, 85, 143);"><u><strong>Bo-Han Chen</strong></u></span>, Yu-Chen Cheng, Shang-Da Yang, Ming-Chang Chen, Chia-Chen Hsu, and A. H. Kung',
+        author:'Chih-Hsuan Lu, Yu-Jung Tsou, Hong-Yu Chen, <span style="color: rgb(31, 85, 143);"><u>Bo-Han Chen</u></span>, Yu-Chen Cheng, Shang-Da Yang, Ming-Chang Chen, Chia-Chen Hsu, and A. H. Kung',
         journal: 'Optica',
         volume: '1',
         issue: '6',
