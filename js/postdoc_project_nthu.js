c0f072268fef6799592fba0e45119958fd202284
Bo-Han Chen <66907587+bchen-tw@users.noreply.github.com>
2023-08-06 01:23:40 +0800

    minor change
    minor change

--- js/postdoc_project_nthu.js
+++ js/postdoc_project_nthu.js
@@ -1,6 +1,7 @@
 var nthu_project_data = [
   
   {
+    isEducating: false,
     isBuilding: false,
     isResearching: false,
     isVisiting: false,
@@ -8,42 +9,49 @@ var nthu_project_data = [
   },
   
   {
+    isEducating: false,
     isBuilding: true,
     isResearching: false,
     isVisiting: false,
     description: 'Designing and building imaging system for UV LED transfer machine.'
   },
   {
+    isEducating: false,
     isBuilding: true,
     isResearching: false,
     isVisiting: false,
-    description: 'Inspecting and fixing a commercial Raman microscope, saving ~1M NTD in the budget for the Institute of Electronics Engineering, NTHU.'
+    description: 'Inspecting and fixing a commercial Raman microscope, saving NTD ~1M in the budget for the Institute of Electronics Engineering, NTHU.'
   },
   {
+    isEducating: false,
     isBuilding: true,
     isResearching: false,
     isVisiting: false,
     description: 'Building a new website for <a href="https://nthu-upl.github.io/" target="_blank">Ultrafast Photonics Lab (UPL)</a> in 3 months with self-taught web designing skills.'
   },
   {
+    isEducating: false,
     isBuilding: true,
     isResearching: false,
     isVisiting: false,
     description: 'Designing and building VIS + NIR spectrometer for full spectrum (450 - 1600 nm) measurement.'
   },
   {
+    isEducating: false,
     isBuilding: true,
     isResearching: false,
     isVisiting: false,
     description: 'Designing and building a prism-based high-speed spectrometer for the visable range (450 - 1000 nm) for ultrafast transient absorption spectroscopy.'
   },
   {
+    isEducating: false,
     isBuilding: true,
     isResearching: false,
     isVisiting: false,
     description: 'Integrating MPC into stimulated Raman scattering for bio-imaging (in collaboration with the Brain Research Center (BRC), NTHU).'
   },
   {
+    isEducating: false,
     isBuilding: true,
     isResearching: false,
     isVisiting: false,
@@ -51,42 +59,49 @@ var nthu_project_data = [
   },
 
   {
+    isEducating: false,
     isBuilding: false,
     isResearching: true,
     isVisiting: false,
     description: 'Improving the Fourier-limit peak power by a factor of 1.8, and generating sub-5 fs pulses with multiple-plate continuum (MPC) via double-pass geometry.'
   },
   {
+    isEducating: false,
     isBuilding: false,
     isResearching: true,
     isVisiting: false,
-    description: 'Simulating spectral broadening behaviors in gas-filled hollow-core fibers (in collaboration with <i>Industrial Technology Research Institute (ITRI)</i>) and providing follow-up technical supports, bringing 600,000 NTD (technical service income) to the Photonics Research Center, NTHU.'
+    description: 'Simulating spectral broadening behaviors in gas-filled hollow-core fibers (in collaboration with <i>Industrial Technology Research Institute (ITRI)</i>) and providing follow-up technical supports, bringing NTD 600,000 (technical service income) to the Photonics Research Center, NTHU.'
   },
   {
+    isEducating: false,
     isBuilding: false,
     isResearching: true,
     isVisiting: false,
     description: 'Studying and demonstrating an energy enhancement by a factor of 5 for MPC with Laguerre Gaussian (vortex / donut-shaped) beams.'
   },
   {
+    isEducating: false,
     isBuilding: false,
     isResearching: true,
     isVisiting: false,
     description: 'Studying the ultrafast dynamics of 2D semiconductor materials (MoS<sub>2</sub>, MoSe<sub>2</sub>, WS<sub>2</sub>, in collaboration with the <i>department of Materials Science and Engineering</i>) and novel light-emitting materials (perovskite and OLED, in collaboration with the <i>department of Chemistry of National Taiwan University (NTU)</i> and the <i>Department of Electronic Engineering of Ming Chi University of Technology</i>).'
   },
   {
+    isEducating: false,
     isBuilding: false,
     isResearching: true,
     isVisiting: false,
     description: 'Studying the effects of laser pulse duration on laser welding strength.'
   },
   {
+    isEducating: false,
     isBuilding: false,
     isResearching: true,
     isVisiting: false,
     description: 'Experimentally studying the physics behind impulsive stimulated Raman scattering (ISRS) and demonstrating the effect of overlapping pump and probe spectra.'
   },
   {
+    isEducating: false,
     isBuilding: false,
     isResearching: true,
     isVisiting: false,
